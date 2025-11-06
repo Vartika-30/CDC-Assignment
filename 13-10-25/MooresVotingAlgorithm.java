fcb69ee0a4405d96f3a04ce9e132a66a0cb0ca94
VARTIKA BHARDWAJ <rasuvartika30@gmail.com>
2025-11-06 21:00:28 +0530

    Implement Moore's Voting Algorithm for majority element

---
+++ 13-10-25/MooresVotingAlgorithm.java
@@ -0,0 +1,33 @@
+package Array;
+
+
+public class MooresVotingAlgorithm {
+
+    
+    public static int majorityElement(int[] nums) {
+      
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        
+        
+        return candidate; 
+    }
+
+    public static void main(String[] args) {
+        int[] data = {2, 2, 1, 1, 1, 2, 2}; 
+        int result = majorityElement(data);
+        System.out.println("Majority Element: " + result);
+    }
+}
